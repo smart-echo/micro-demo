efebe7f299388f60598db9d91b9f84b54149442d
阿旭 <143504562+dongxumei@users.noreply.github.com>
2024-06-27 10:04:10 +0000

    add hello service

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@
 go install github.com/smart-echo/micro-toolkit/cmd/micro@latest
 
 source <(micro completion bash)
+
+micro new service github.com/smart-echo/micro-demo/hello && cd hello && make init proto update tidy && cd -
+
+git add . && git commit -m "add hello service" && git push
