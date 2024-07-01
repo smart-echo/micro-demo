7b8db5a736ebc4d57d0dd2dd04a8eee49e3d1f6a
阿旭 <143504562+dongxumei@users.noreply.github.com>
2024-07-01 07:54:31 +0000

    upgrade hello service, support trace

--- upgrade.sh
+++ upgrade.sh
@@ -6,9 +6,9 @@ source <(micro completion bash)
 
 rm -rf hello && hello-client
 
-micro new service github.com/smart-echo/micro-demo/hello && cd hello && make init proto update tidy && cd -
+micro new service --trace github.com/smart-echo/micro-demo/hello && cd hello && make init proto update tidy && cd -
 
-git add . && git commit -m "upgrade hello service" && git push
+git add . && git commit -m "upgrade hello service, support trace" && git push
 
 micro new client github.com/smart-echo/micro-demo/hello && cd hello-client && make update tidy && cd -
 
